6bce40a1d7d8a1f5c4a83bd54bb3d4269a0c38b3
Talon Daniels <talondaniels@gmail.com>
2017-08-04 20:54:39 -0600

    Add canned image support to remote-print command

--- paperwaster/cli/remote_print.py
+++ paperwaster/cli/remote_print.py
@@ -10,14 +10,22 @@ def main():
     ap.add_argument('--message', '-m', help='message to print')
     ap.add_argument('--font', '-f', help='name of the font to use', default='hack')
     ap.add_argument('--size', '-s', help='size of font', default=24, type=int)
+    ap.add_argument('--image-code', '-ic', help='image code to print')
     ap.add_argument('--image', '-i', help='image code to print')
     args = ap.parse_args()
     config = yaml.safe_load(open(args.config))
     if args.message:
         publish_message(args.message, args.font, args.size, redis_uri=config['redis_uri'])
 
+    if args.image_code:
+        publish_image_code(args.image_code, redis_uri=config['redis_uri'])
+    
     if args.image:
-        publish_image_code(args.image, redis_uri=config['redis_uri'])
+        ic = config.get('images', {}).get(args.image)
+        if ic:
+            publish_image_code(ic, redis_uri=config['redis_uri'])
+        else:
+            print('No image code for {}'.format(args.image))
 
 if __name__ == '__main__':
     main()
