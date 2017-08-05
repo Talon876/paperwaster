ef49a88e4e11e82b3810a09f8657d7b35f4566cb
Talon Daniels <talondaniels@gmail.com>
2017-08-04 22:06:02 -0600

    Trim out extra whitespace

--- paperwaster/converter.py
+++ paperwaster/converter.py
@@ -35,7 +35,7 @@ def text_to_image(msg, font=None, font_path=None, font_size=24, accurate=True):
         y += h
     return img
 
-def code_to_image(image_code, pixel_size=8):
+def code_to_image(image_code, pixel_size=8, trim=True):
     image_data = ''.join([str(bin(int(s, 16)))[2:].zfill(32) for s in image_code.split('-')])
     num_rows = len(image_data) / (PRINTER_WIDTH / pixel_size)
     img = Image.new('1', (PRINTER_WIDTH, num_rows * pixel_size), color='white')
@@ -46,7 +46,17 @@ def code_to_image(image_code, pixel_size=8):
             if cell == '1':
                 x, y = tx*pixel_size, ty*pixel_size
                 draw.rectangle([x, y, x + pixel_size, y + pixel_size], fill='black')
-    return img
+    return trim_whitespace(img) if trim else img
+
+def trim_whitespace(img):
+    w, h = img.size
+    top_row, btm_row = 0, 0
+    # loop through each row, looking for the first and last rows with a black pixel
+    for y in range(h):
+        if 0 in [img.getpixel((x, y)) for x in range(w)]:
+            top_row = y if top_row == 0 else top_row
+            btm_row = y
+    return img.crop((0, top_row, w, btm_row))
 
 def _chunks(l, n):
     for i in range(0, len(l), n):
