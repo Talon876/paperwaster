ed0ed73f8dcf07c2f6d0058ccf95a45df15a4d90
Talon Daniels <talondaniels@gmail.com>
2017-08-10 20:47:29 -0600

    Add date filter to image export script

--- paperwaster/cli/export_images.py
+++ paperwaster/cli/export_images.py
@@ -4,6 +4,7 @@ import traceback
 from paperwaster.converter import code_to_image
 from paperwaster.web.app import db, create_app
 from paperwaster.models import User, Command
+from dateutil import parser
 import argparse
 import json
 
@@ -11,12 +12,14 @@ def main():
     ap = argparse.ArgumentParser()
     ap.add_argument('--config', '-c', help='path to config file', default='config.yaml')
     ap.add_argument('--out', '-o', help='directory to output images to', default='imgs')
+    ap.add_argument('--after', '-a', help='filter to images after this date', required=True)
     args = ap.parse_args()
     if not os.path.isdir(args.out):
         os.makedirs(args.out)
     with create_app(args.config).app_context():
-        img_codes = Command.query.filter(Command.cmd=='image').all()
-        print('Exporting {} images'.format(len(img_codes)))
+        after_date = parser.parse(args.after)
+        img_codes = Command.query.filter(Command.cmd=='image', Command.timestamp>=after_date).all()
+        print('Exporting {} images after {}'.format(len(img_codes), after_date.isoformat()))
         for c in img_codes:
             try:
                 img_code = json.loads(c.data)['code']
@@ -24,6 +27,8 @@ def main():
                 if not os.path.exists(fn):
                     img = code_to_image(img_code)
                     img.save(fn)
+                else:
+                    print('{} already exists - skipping'.format(fn))
             except Exception:
                 traceback.print_exc()
                 print('Failed to convert image code {}'.format(img_code))
