523a19e37b6497d4b5d8a3fe0c3bb60fedf1354c
Talon Daniels <talondaniels@gmail.com>
2017-08-11 19:41:24 -0600

    Split out fading-message component and other Vue stuff

--- paperwaster/web/static/app.js
+++ paperwaster/web/static/app.js
@@ -1,17 +1,26 @@
+Vue.component('fading-message', {
+    template: `
+    <div style="display:inline-block">
+        <transition mode="out-in" enter-active-class="animated fadeIn" leave-active-class="animated fadeOut">
+            <span :key="message" class="donation-message">{{message}}</span>
+        </transition>
+    </div>`,
+    props: ['message']
+});
+
 Vue.component('donate-message', {
-    template: `<div>Please consider a
-               <a v-bind:href="link" class="btn btn-xs btn-success">small donation</a>
-               <transition mode="out-in" enter-active-class="animated fadeIn" leave-active-class="animated fadeOut" >
-                 <span v-on:click="updateReason" v-bind:key="reason" class="donation-message"> {{reason}}</span>
-               </transition>
+    template: `<div>
+                 Please consider a <a :href="link" class="btn btn-xs btn-success">small donation </a>
+                 <fading-message class="donation-message" @click.native="updateReason" :message="reason"/>
                </div>`,
-    props: ['reasons'],
+    props: ['reasons', 'delay', 'initialDelay', 'donateUrl'],
     data: function() {return {
         reason: null
     }},
     computed: {
         link: function () {
-            return this.reason === null ? '/donate' : '/donate?msg=' + encodeURIComponent(this.reason);
+            return this.reason === null ?
+                   this.donateUrl : this.donateUrl + '?msg=' + encodeURIComponent(this.reason);
         }
     },
     methods: {
@@ -22,14 +31,17 @@ Vue.component('donate-message', {
             }
             this.reason = reason;
         }
+    },
+    created: function() {
+        var self = this;
+        var updateReasonLoop = function() {
+            self.updateReason();
+            setTimeout(updateReasonLoop, self.delay);
+        };
+        setTimeout(updateReasonLoop, this.initialDelay);
     }
 });
-var vm = new Vue({
+
+new Vue({
     el: '#donateApp',
 });
-
-var updateDonationReasonTimer = function () {
-    vm.$refs.donateMessage.updateReason();
-    setTimeout(updateDonationReasonTimer, 60 * 1000);
-};
-setTimeout(updateDonationReasonTimer, 30*1000);
